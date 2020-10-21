38f76b1189871959d7232845a9c1802a9a0b76ba
sajoum <71432592+sajoum@users.noreply.github.com>
2020-10-21 22:17:38 +0200

    Delete kladd3.py

--- kladd3.py
+++
@@ -1,9 +0,0 @@
-
-
-if __name__ == '__main__':
-    try:
-        number = int(input("Hur många frågor? "))
-        max_value = int(input("Största tal? "))
-        game(number, max_value)
-    except:
-        print("Oj, här blev det fel!")
