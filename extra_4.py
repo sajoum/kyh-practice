9ac9272b22e9d6f0a182f8aa28989abc3983ae72
sajoum <71432592+sajoum@users.noreply.github.com>
2020-10-21 22:21:45 +0200

    Delete extra_4.py

--- extra_4.py
+++
@@ -1,8 +0,0 @@
-
-char = input("Write a character: ")
-vowels = "aeiouy"
-
-if char.lower() in vowels:
-    print(f"The entered letter {char} is a vowel")
-else:
-    print(f"Letter {char} is not a vowel")
