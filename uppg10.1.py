9809a03e7092489b35dba09f3961897e0b470531
Samira <samira.joumal@student.kyh.se>
2020-10-21 22:06:13 +0200

    uppg10.1

--- uppg10.1.py
+++ uppg10.1.py
@@ -1,9 +1,5 @@
 '''10. Skriv flera program (t.ex ug10_1.py, ug10_2.py osv) som skriver ut:
-1) heltalen fr.o.m. 1 t.o.m. 10 i ökande ordning
-2) udda tal fr.o.m. 1 t.o.m. 49 i ökande ordning
-3) heltal fr.o.m. 10 t.o.m. 1 i minskande ordning
-4) summan av talen 7 t.o.m 1000
-5) produkten av talen 1 t.o.m 1000'''
+1) heltalen fr.o.m. 1 t.o.m. 10 i ökande ordning'''
 
-for i in range(1,11):
+for i in range(1, 11):
     print(i)
