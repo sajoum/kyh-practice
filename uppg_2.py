c1248aa15625461f474acbec651ab6f9a422b592
sajoum <71432592+sajoum@users.noreply.github.com>
2020-10-21 22:26:06 +0200

    Delete uppg_2.py

--- uppg_2.py
+++
@@ -1,8 +0,0 @@
-'''# Fråga användaren "What is your name?" och lagra svaret i en variabel med namnet "name"
-name = input("What is your name?")
-
-# Slå ihop textsträngen "Your name is: " och svaret som lagrats i variabeln "name", och skriv ut resultatet på skärmen
-print("Your name is: " + name)'''
-
-name = input("What is your name? ")
-print("Your name is: " + name)
